73c5545aaa1f350babffe41836199419528430a0
Leticia <letisaless@gmail.com>
2020-04-04 19:16:01 -0300

    added login and dashboard component and moved to webpack
    Signed-off-by: Leticia <letisaless@gmail.com>

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid" style="padding: 0px;">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  components: {},
+  data () {
+    return {
+      title: 'App',
+      user: (this.$route.params.user) ? this.$route.params.user : null
+    }
+  }
+}
+</script>
---
+++ client/src/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+<div class='tab-pane fade show active' id='pills-login' role='tabpanel' aria-labelledby='pills-login-tab'>
+  <div class='row'>
+    <div class='col-md-12'>
+        <form @submit.prevent='login'>
+            <div class='form-group'>
+                <label for=''>Email:</label>
+                <input type='email' required class='form-control' placeholder='eg chris@invoiceapp.com' v-model='model.email'>
+            </div>
+
+            <div class='form-group'>
+                <label for=''>Password:</label>
+                <input type='password' required class='form-control' placeholder='Enter Password' v-model='model.password'>
+            </div>
+
+            <div class='form-group'>
+                <button class='btn btn-primary' >Login</button>
+                {{ loading }}
+                {{ status }}
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Login',
+  components: {},
+  data () {
+    return {
+      model: {
+        email: '',
+        password: ''
+      },
+      loading: '',
+      status: ''
+    }
+  },
+  methods: {
+    login () {
+      const formData = new FormData()
+      formData.append('email', this.model.email)
+      formData.append('password', this.model.password)
+      this.loading = 'Signing in'
+      // Post to server
+      axios.post('http://localhost:3128/login', formData).then(res => {
+        // Post a status message
+        this.loading = ''
+        if (res.data.status === true) {
+          // now send the user to the next route
+          this.$router.push({
+            name: 'Dashboard',
+            params: { user: res.data.user }
+          })
+        } else {
+          this.status = res.data.message
+        }
+      })
+    }
+  }
+}
+</script>
